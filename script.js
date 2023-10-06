6535d6f4dc5ee042a337884330d0b34a5ed375c4
AyushGupta7005 <12321ayushgupta@gmail.com>
2023-10-06 19:44:28 +0530

    Local storage added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let run = false;
+let isStarted = false;
 let isRunning = false;
 let milliSecond = 0;
 let second = 0;
@@ -7,11 +7,22 @@ let hour = 0;
 let timer = false;
 let lap = "";
 let lapCount = 0;
+let record=[];
+
 document.getElementById("lapReset").disabled = true;
 
+if (localStorage.length != 0) {
+    document.getElementById("lapHead").innerHTML = "LAPS" + "<br>";
+    lapCount = localStorage.getItem("lapCount");
+    restringRecord=window.localStorage.getItem("key");
+    record=JSON.parse(restringRecord);
+}
+for (let i = 0; i < lapCount; i++) {
+    document.getElementById("laps").innerHTML= (record[i] + "<br>")+document.getElementById("laps").innerHTML;
+}
 function toStart() {
     isRunning = true;
-    run = true;
+    isStarted = true;
     timer = true;
     document.getElementById("startStopResume").textContent = "Stop";
     document.getElementById("lapReset").disabled = false;
@@ -27,7 +38,7 @@ function toStop() {
     document.getElementById("lapReset").textContent = "Reset";
 }
 function toResume() {
-    run = true;
+    isStarted = true;
     timer = true;
     isRunning = true;
     document.getElementById("startStopResume").classList.replace("blue", "red");
@@ -37,18 +48,22 @@ function toResume() {
 }
 
 function toLap() {
-
     if (lapCount == 0) {
-        document.getElementById("laps").innerHTML = "LAPS" + "<br>" + "<br>";
+        document.getElementById("lapHead").innerHTML = "LAPS" + "<br>";
     }
     lapCount++;
-    lap = lapCount + ") " + hour + ":" + minute + ":" + second + "." + milliSecond;
-    document.getElementById("laps").innerHTML += (lap + "<br>");
+
+    lap = hour + ":" + minute + ":" + second + "." + milliSecond;
+    document.getElementById("laps").innerHTML = (lap + "<br>") + document.getElementById("laps").innerHTML;
+    record[lapCount-1]=lap;
+    stringRecord=JSON.stringify(record);
+    window.localStorage.setItem("key",stringRecord);
+    window.localStorage.setItem("lapCount",lapCount);
 }
 
 function toReset() {
     timer = false;
-    run = false;
+    isStarted = false;
     milliSecond = 0;
     second = 0;
     minute = 0;
@@ -59,17 +74,19 @@ function toReset() {
     document.getElementById("second").innerHTML = "00";
     document.getElementById("millisecond").innerHTML = "00";
     document.getElementById("laps").innerHTML = "";
+    document.getElementById("lapHead").innerHTML = "";
     document.getElementById("startStopResume").textContent = "Start";
     document.getElementById("lapReset").textContent = "Lap";
     document.getElementById("lapReset").disabled = true;
     document.getElementById("startStopResume").classList.replace("blue", "purple");
+    window.localStorage.clear();
 }
 
 function startStopResume() {
-    if (isRunning == false && run == false) {
+    if (isRunning == false && isStarted == false) {
         toStart();
     }
-    else if (isRunning == true && run == true) {
+    else if (isRunning == true && isStarted == true) {
         toStop();
     }
     else {
@@ -103,17 +120,38 @@ function timing() {
             minute = 0;
             hour++;
         }
-        document.getElementById("hour").innerHTML = hour;
-        document.getElementById("minute").innerHTML = minute;
-        document.getElementById("second").innerHTML = second;
-        document.getElementById("millisecond").innerHTML = milliSecond;
+
+        if (hour < 10) {
+            document.getElementById("hour").innerHTML = "0" + hour;
+        }
+        else {
+            document.getElementById("hour").innerHTML = "" + hour;
+        }
+        if (minute < 10) {
+            document.getElementById("minute").innerHTML = "0" + minute;
+        }
+        else {
+            document.getElementById("minute").innerHTML = "" + minute;
+        }
+        if (second < 10) {
+            document.getElementById("second").innerHTML = "0" + second;
+        }
+        else {
+            document.getElementById("second").innerHTML = "" + second;
+
+        }
+        if (milliSecond < 10) {
+            document.getElementById("millisecond").innerHTML = "0" + milliSecond;
+        }
+        else {
+            document.getElementById("millisecond").innerHTML = "" + milliSecond;
+        }
         setTimeout("timing()", 10);
-        window.addEventListener("keydown", checkkey);
     }
 }
 
-document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "s") { toStart() } });
-document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "x") { toStop() } });
-document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "r") { toReset() } });
-document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "l") { toLap() } });
-document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "p") { toResume() } });
+document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "s") { toStart(); e.preventDefault(); } });
+document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "x") { toStop(); e.preventDefault(); } });
+document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "r") { toReset(); e.preventDefault(); } });
+document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "l") { toLap(); e.preventDefault(); } });
+document.addEventListener("keydown", e => { if (e.ctrlKey && e.key.toLowerCase() === "p") { toResume(); e.preventDefault(); } });
